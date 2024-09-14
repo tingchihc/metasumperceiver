f5e4792c144270c2bfeab6900ffe4754436d9f4d
Mike <91083830+tingchihc@users.noreply.github.com>
2024-09-14 15:07:13 -0400

    Add files via upload

---
+++ dataset/search_img.py
@@ -0,0 +1,80 @@
+import json
+import random
+import os
+import time
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def get_soup(url):
+    response = requests.get(url, headers = {'User-Agent': random.choice(USER_AGENTS)})
+    html_content = response.text
+    return BeautifulSoup(html_content,'html.parser')
+
+def find_jpgs(soup):
+    lists = []
+    img_tags = soup.find_all('img')
+    for img_tag in img_tags:
+        src = img_tag.get('src')
+        lists.append(src)
+    return lists
+
+def download_first_img(img_array, filename):
+    for i in img_array:
+        if 'gif' not in i:
+            try:
+                img_data = requests.get(i).content
+                with open(filename, 'wb') as img_file:
+                    img_file.write(img_data)
+                return True
+                break
+            except:
+                continue
+
+
+USER_AGENTS = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    #Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
+DATA = 'documents.json'
+with open(DATA, 'r') as f:
+    data = json.load(f)
+
+for k, v in data.items():
+    subset = k
+
+    for kk, vv in data[subset].items():
+        query = vv
+        url = "https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+        soup = get_soup(url)
+        img_array = find_jpgs(soup)
+
+        current_path = os.getcwd()
+        filename = current_path + '/' + subset + '_' + kk + '.jpg'
+        print(filename)
+        download_first_img(img_array, filename)
+        
